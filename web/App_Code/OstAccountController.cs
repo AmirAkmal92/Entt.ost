b7f84112eda10d2dac2c13aba1ef3d59dbefef3e
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2016-10-24 18:59:56 +0800

    Custom Shell Debut

---
+++ web/App_Code/OstAccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using Bespoke.Sph.Domain;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace web.sph.App_Code
+{
+    [RoutePrefix("ost-account")]
+    public class OstAccountController : Controller
+    {
+        [AllowAnonymous]
+        [Route("login")]
+        public ActionResult Login()
+        {
+            //Todo: to be implemented
+            return View();
+        }
+
+        [Authorize]
+        [Route("change-password")]
+        public ActionResult ChangePassword()
+        {
+            //Todo: to be implemented
+            return View();
+        }
+
+        [AllowAnonymous]
+        [Route("forgot-password")]
+        public ActionResult ForgotPassword()
+        {
+            //Todo: to be implemented
+            return View();
+        }
+
+        [AllowAnonymous]
+        [Route("reset-password/{id}")]
+        public ActionResult ResetPassword(string id)
+        {
+            //Todo: to be implemented
+            return View();
+        }
+    }
+}
