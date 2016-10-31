ddf961d2ee9ad7f217a4025e8d8989a1ea351e45
erymuzuan <erymuzuan@hotmail.com>
2016-11-01 06:11:56 +0800

    Update in a while lopp until the lasters releases

--- RunControlCenter.ps1
+++ RunControlCenter.ps1
@@ -53,58 +53,80 @@ if((Test-Path(".\version.json")) -eq $false)
 $updateJson | Out-File .\version.json -Encoding ascii
 }
 
-$json = Get-Content .\version.json | ConvertFrom-Json
-$build = $json.build
-Write-Host "Please wait while we check for new RX build, current build is $build" -ForegroundColor Cyan
 
-try{
-    $release = Invoke-WebRequest -Method Get -UseBasicParsing -Uri "$UpdateUrl/binaries/$build.json" -ErrorAction Ignore
-    Write-Host $release.StatusCode
 
-    $jsonResponse = $release.Content | ConvertFrom-Json
-    $vnext = $jsonResponse.vnext;
-    # $updateScript = $jsonResponse["update-script"]
-    #download update script
-    Write-Host "There's a new update package, $vnext is available " -ForegroundColor Yellow
-    Write-Host "Do you want to apply this update[Yes(y), No (n)] : " -ForegroundColor Yellow -NoNewline
+while($true){
+
+
+    $json = Get-Content .\version.json | ConvertFrom-Json
+    $build = $json.build
+    Write-Host "Please wait while we check for new RX build, current build is $build" -ForegroundColor Cyan
+
+    try{
+        $release = Invoke-WebRequest -Method Get -UseBasicParsing -Uri "$UpdateUrl/binaries/$build.json" -ErrorAction Ignore
+        Write-Host $release.StatusCode
+
+        $jsonResponse = $release.Content | ConvertFrom-Json
+        $vnext = $jsonResponse.vnext;
+        # $updateScript = $jsonResponse["update-script"]
+        #download update script
+        Write-Host "There's a new update package, $vnext is available " -ForegroundColor Yellow
+        Write-Host "Do you want to apply this update[Yes(y), No (n)] : " -ForegroundColor Yellow -NoNewline
  
-    $applyUpdate = Read-Host
-    if($applyUpdate -eq 'y')
-    {        
-        if((Test-Path(".\$vnext")) -eq $true)
-        {
-            Remove-Item ".\$vnext" -Force -Recurse
-        }
-        Write-Host "Downloading $UpdateUrl/binaries/$vnext/$vnext.ps1"
+        $applyUpdate = Read-Host
+        if($applyUpdate -eq 'y')
+        {        
+            if((Test-Path(".\$vnext")) -eq $true)
+            {
+                Remove-Item ".\$vnext" -Force -Recurse
+            }
+            Write-Host "Downloading $UpdateUrl/binaries/$vnext/$vnext.ps1"
 
-        if((Test-Path(".\$vnext.ps1")) -eq $true){
-            Remove-Item ".\$vnext.ps1"
-        }
+            if((Test-Path(".\$vnext.ps1")) -eq $true){
+                Remove-Item ".\$vnext.ps1"
+            }
 
-        Invoke-WebRequest -Method Get -UseBasicParsing -Uri "$UpdateUrl/binaries/$vnext/$vnext.ps1" -OutFile ".\$vnext.ps1"
+            Invoke-WebRequest -Method Get -UseBasicParsing -Uri "$UpdateUrl/binaries/$vnext/$vnext.ps1" -OutFile ".\$vnext.ps1"
 
-        # run 
-        Write-Host "Runing... ./$vnext.ps1"
-        & ".\$vnext.ps1"
+            # run 
+            Write-Host "Runing... ./$vnext.ps1"
+            & ".\$vnext.ps1"
         
-        Write-Host "Successfully applying the update, please check your git status, see if there's any error"
+            Write-Host "Successfully applying the update, please check your git status, see if there's any error"
         
-        if($KeepDownloadedUpdates.IsPresent -eq $false)
-        {
-            # remove the folder and scripts
-            Remove-Item ".\$vnext.ps1"
-            Remove-Item ".\$vnext" -Recurse -Force
-        }        
+            if($KeepDownloadedUpdates.IsPresent -eq $false)
+            {
+                # remove the folder and scripts
+                Remove-Item ".\$vnext.ps1"
+                Remove-Item ".\$vnext" -Recurse -Force
+            }
+            $today = [System.DateTime]::Today.ToString("yyyy-MM-dd");
+            
+                $updateJson = @"
+{   
+    "build": $vnext,
+    "date" : "$today"
+}
+"@
+$updateJson | Out-File .\version.json -Encoding ascii        
+        }
+
+
+    }catch{
+     $code = $_.Exception.Response.StatusCode.Value__
+     if($code -eq 404){
+        Write-Host "No update is avalailable" -ForegroundColor Cyan
+        Write-Host "Starting control center"
+     }
+     break;
     }
 
 
-}catch{
- $code = $_.Exception.Response.StatusCode.Value__
- if($code -eq 404){
-    Write-Host "No update is avalailable" -ForegroundColor Cyan
-    Write-Host "Starting control center"
- }
+
+
 }
+
+
 
 
 & .\control.center\controlcenter.exe
