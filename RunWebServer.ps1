d9bbb78a7d84e63a03bf77a982749e6b5b24224e
erymuzuan <erymuzuan@hotmail.com>
2016-11-06 15:46:57 +0800

    Get the LoggerWebSocket port value, requies 10335

--- RunWebServer.ps1
+++ RunWebServer.ps1
@@ -1,1 +1,20 @@
-﻿Start-Process -FilePath '.\IIS Express\iisexpress.exe' -ArgumentList @("/config:.\config\applicationhost.config",  "/site:web.Ost",  "/trace:error")
+﻿param
+(
+    [int]$LoggerWebSocketPort = 50238
+)
+& .\env.ost.ps1
+
+Import-Module .\utils\sqlcmd.dll
+
+$port = Get-Process controlcenter | Get-ProcessVariable -Variable RX_OST_LoggerWebSocketPort
+$port1 = $LoggerWebSocketPort
+if([System.Int32]::TryParse($port, [ref]$port1)){
+    Write-Host "Getting WebSocket port value from controlcenter" -ForegroundColor Cyan
+    Write-Host "Setting RX_OST_LoggerWebSocketPort value from ControlCenter : $port1"
+    $env:RX_OST_LoggerWebSocketPort = $port1
+}else{
+    Write-Host "Setting RX_OST_LoggerWebSocketPort value from parameter : $LoggerWebSocketPort"
+    $env:RX_OST_LoggerWebSocketPort = $LoggerWebSocketPort
+}
+Start-Process -FilePath '.\IIS Express\iisexpress.exe' -ArgumentList @("/config:.\config\applicationhost.config",  "/site:web.Ost",  "/trace:error")
+
