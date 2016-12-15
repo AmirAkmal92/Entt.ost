31d2da8811d608c775091112c9aaa51cd889978d
Mohammad Abd Razak <mohammad@bespoke.com.my>
2016-12-15 11:12:10 +0800

    Rate and reference

--- web/Scripts/_shipping.rate.js
+++ web/Scripts/_shipping.rate.js
@@ -1,58 +1,58 @@
 ﻿$(function () {
     var ost = {};
     ost.ShippingRateModel = function () {
-            var busy = ko.observable(false),
-                fromAddress = ko.observable(),
-                fromPostcode = ko.observable(),
-                toAddress = ko.observable(),
-                toPostcode = ko.observable(),
-                weight = ko.observable(),
-                width = ko.observable(),
-                height = ko.observable(),
-                length = ko.observable(),
-                valueAddedServices = ko.observableArray(),
-                estimatedTotal = ko.observable(),
-                products = ko.observableArray(),
-                getRatesAsync = function () {
-                    busy(true);
-                    return $.getJSON("snb-services/products/?from=" + ko.unwrap(fromPostcode) + "&to=" + ko.unwrap(toPostcode) + "&country=MY&weight=" +
-                            ko.unwrap(weight) + "&height=" + ko.unwrap(height) + "&length=" + ko.unwrap(length) + "&width=" + ko.unwrap(width))
-                        .then(products)
-                        .done(function(){
-                            busy(false);
-                        });
-                };
-
-            return {
-                fromAddress: fromAddress,
-                fromPostcode: fromPostcode,
-                toAddress: toAddress,
-                toPostcode: toPostcode,
-                weight: weight,
-                width: width,
-                height: height,
-                length: length,
-                valueAddedServices: valueAddedServices,
-                estimatedTotal: estimatedTotal,
-                getRatesAsync: getRatesAsync,
-                products : products,
-                busy : busy
+        var busy = ko.observable(false),
+            fromAddress = ko.observable(),
+            fromPostcode = ko.observable(),
+            toAddress = ko.observable(),
+            toPostcode = ko.observable(),
+            weight = ko.observable(),
+            width = ko.observable(),
+            height = ko.observable(),
+            length = ko.observable(),
+            valueAddedServices = ko.observableArray(),
+            estimatedTotal = ko.observable(),
+            products = ko.observableArray(),
+            getRatesAsync = function () {
+                busy(true);
+                return $.getJSON("ost/snb-services/products/?from=" + ko.unwrap(fromPostcode) + "&to=" + ko.unwrap(toPostcode) + "&country=MY&weight=" +
+                        ko.unwrap(weight) + "&height=" + ko.unwrap(height) + "&length=" + ko.unwrap(length) + "&width=" + ko.unwrap(width))
+                    .then(products)
+                    .done(function () {
+                        busy(false);
+                    });
             };
-        },
+
+        return {
+            fromAddress: fromAddress,
+            fromPostcode: fromPostcode,
+            toAddress: toAddress,
+            toPostcode: toPostcode,
+            weight: weight,
+            width: width,
+            height: height,
+            length: length,
+            valueAddedServices: valueAddedServices,
+            estimatedTotal: estimatedTotal,
+            getRatesAsync: getRatesAsync,
+            products: products,
+            busy: busy
+        };
+    },
         formatRepo = function (address) {
             if (!address) return "";
             if (address.loading) return address.text;
-            var markup = "<div class='form-group'>" +
-                "<div class='form-group'><img src='/assets/layouts/layout/img/avatar3_small.jpg' /></div>" +
-                "<div class='form-group'>" +
-                "<div class='form-group'>" + address.Location + "</div>";
+            var markup = "<div class='select2-result-repository clearfix'>" +
+                "<div class='select2-result-repository__avatar'><img src='/assets/layouts/layout/img/avatar3_small.jpg' /></div>" +
+                "<div class='select2-result-repository__meta'>" +
+                "<div class='select2-result-repository__title'>" + address.Location + "</div>";
 
-            markup += "<div class='form-group'>" + address.City + "</div>";
+            markup += "<div class='select2-result-repository__description'>" + address.City + "</div>";
 
 
-            markup += "<div class='form-group'>" +
-                "<div class='form-group'></i> " + address.State + " </div>" +
-                "<div class='form-group'><i class='fa fa-star'></i> " + address.Postcode + " </div>" +
+            markup += "<div class='select2-result-repository__statistics'>" +
+                "<div class='select2-result-repository__forks'><i class='fa fa-flash'></i> " + address.State + " </div>" +
+                "<div class='select2-result-repository__stargazers'><i class='fa fa-star'></i> " + address.Postcode + " </div>" +
                 "</div>" +
                 "</div></div>";
 
@@ -62,44 +62,44 @@
 
 
     $("#to-address, #from-address").select2({
-            ajax: {
-                url: "/api/address-lookups/",
-                dataType: 'json',
-                delay: 250,
-                data: function (params) {
-                    return {
-                        q: params.term.replace(/\//g, '\\/'), // search term
-                        page: params.page
-                    };
-                },
-                processResults: function (data, params) {
-                    params.page = params.page || 1;
-                    var results = _(data._results).map(function (v) {
-                        v.id = v.Id;
-                        return v;
-                    });
-                    return {
-                        results: results,
-                        pagination: {
-                            more: (params.page * 30) < data._count
-                        }
-                    };
-                },
-                cache: false
+        ajax: {
+            url: "/api/address-lookups/",
+            dataType: 'json',
+            delay: 250,
+            data: function (params) {
+                return {
+                    q: params.term.replace(/\//g, '\\/'), // search term
+                    page: params.page
+                };
             },
-            escapeMarkup: function (markup) {
-                return markup;
+            processResults: function (data, params) {
+                params.page = params.page || 1;
+                var results = _(data._results).map(function (v) {
+                    v.id = v.Id;
+                    return v;
+                });
+                return {
+                    results: results,
+                    pagination: {
+                        more: (params.page * 30) < data._count
+                    }
+                };
             },
-            minimumInputLength: 3,
-            templateResult: formatRepo,
-            templateSelection: function (o) {
-                if (o.Location && o.Postcode) {
-                    return o.Location + ", " + o.Postcode + " " + o.City + ", " + o.State;
-                }
-                return o.text;
-                //return o.Postcode;
+            cache: false
+        },
+        escapeMarkup: function (markup) {
+            return markup;
+        },
+        minimumInputLength: 3,
+        templateResult: formatRepo,
+        templateSelection: function (o) {
+            if (o.Location && o.Postcode) {
+                return o.Location + ", " + o.Postcode + " " + o.City + ", " + o.State;
             }
-        })
+            return o.text;
+            //return o.Postcode;
+        }
+    })
         .on("select2:select", function (e) {
             var address = e.params.data;
             if (!address) {
