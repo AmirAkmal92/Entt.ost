e8c95939240f48ad14cbd7d77e0350f5c736356e
Mohammad Abd Razak <mohammad@bespoke.com.my>
2016-12-24 14:39:45 +0800

    next bulkk

--- web/SphApp/viewmodels/bulk.receiver.dialog.js
+++ web/SphApp/viewmodels/bulk.receiver.dialog.js
@@ -1,15 +1,10 @@
-﻿define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router,
-objectbuilders.system, objectbuilders.validation, objectbuilders.eximp,
-objectbuilders.dialog, objectbuilders.watcher, objectbuilders.config,
-objectbuilders.app, "plugins/dialog"],
+﻿define([objectbuilders.datacontext, objectbuilders.system, "plugins/dialog"],
 
-function (context, logger, router, system, validation, eximp, dialog, watcher, config, app, dialog) {
+function (context, system, dialog) {
 
     var receiver = ko.observable(new bespoke.Ost_consigmentRequest.domain.Receiver(system.guid())),
-        watching = ko.observable(false),
         id = ko.observable(),
         i18n = null,
-
         activate = function (con) {
             receiver(new bespoke.Ost_consigmentRequest.domain.Receiver(system.guid()));
             return true;
@@ -20,9 +15,9 @@ function (context, logger, router, system, validation, eximp, dialog, watcher, c
             var markup = "<div class='select2-result-repository clearfix'>" +
               "<div class='select2-result-repository__avatar'><img src='/assets/layouts/layout/img/avatar3_small.jpg' /></div>" +
               "<div class='select2-result-repository__meta'>" +
-                "<div class='select2-result-repository__title'>" + contact.CompanyName + "</div>";
+                "<div class='select2-result-repository__title'>" + contact.ContactPerson + "</div>";
 
-            markup += "<div class='select2-result-repository__description'>" + contact.ContactPerson + "</div>";
+            markup += "<div class='select2-result-repository__description'>" + contact.CompanyName + "</div>";
 
 
             markup += "<div class='select2-result-repository__statistics'>" +
@@ -30,12 +25,11 @@ function (context, logger, router, system, validation, eximp, dialog, watcher, c
               "<div class='select2-result-repository__stargazers'><i class='fa fa-star'></i> " + contact.ContactInformation.EmailAddress + " Email</div>" +
               "<div class='select2-result-repository__watchers'><i class='fa fa-eye'></i> " + contact.ContactInformation.PhoneNumber + " Phone</div>" +
             "</div>" +
-            "</div></div>";
+            "</div>";
 
             return markup;
         },
         attached = function (view) {
-
             $("#receiver-company-name").select2({
                 ajax: {
                     url: "/api/address-books/",
@@ -94,28 +88,15 @@ function (context, logger, router, system, validation, eximp, dialog, watcher, c
         },
 
         okClick = function (data, ev) {
-            //if (bespoke.utils.receiver.checkvalidity(ev.target)) {
                 dialog.close(this, "OK");
-            //}
         },
         cancelClick = function () {
             dialog.close(this, "Cancel");
-        },
-        compositionComplete = function () {
-            $("[data-i18n]").each(function (i, v) {
-                var $label = $(v),
-                    text = $label.data("i18n");
-                if (i18n && typeof i18n[text] === "string") {
-                    $label.text(i18n[text]);
-                }
-            });
         };
     var vm = {
         activate: activate,
         attached: attached,
-        config: config,
         receiver: receiver,
-        compositionComplete: compositionComplete,
         okClick: okClick,
         cancelClick: cancelClick
     };
