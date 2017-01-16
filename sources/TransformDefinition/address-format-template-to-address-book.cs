fa609436f896783e078714aca1e21951b32f34bb
Mohammad Abd Razak <mohammad@bespoke.com.my>
2017-01-17 03:20:54 +0800

    Try new receive port

---
+++ sources/TransformDefinition/address-format-template-to-address-book.cs
@@ -0,0 +1,19 @@
+
+namespace Bespoke.Ost.Integrations.Transforms
+{
+    public partial class AddressFormatTemplateToAddressBook
+    {
+
+
+        partial void BeforeTransform(Bespoke.Ost.AddressFormats.Domain.AddressFormat item, Bespoke.Ost.AddressBooks.Domain.AddressBook destination)
+        {
+
+        }
+
+        partial void AfterTransform(Bespoke.Ost.AddressFormats.Domain.AddressFormat item, Bespoke.Ost.AddressBooks.Domain.AddressBook destination)
+        {
+
+        }
+
+    }
+}
