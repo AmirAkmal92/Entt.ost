5aa51f6485ce69188faaea9ad691b4e8f893360f
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-01-16 18:17:08 +0800

    brush up Address Book functionalities

--- web/SphApp/viewmodels/_address-book-groups.js
+++ web/SphApp/viewmodels/_address-book-groups.js
@@ -4,31 +4,26 @@ define(['services/datacontext', 'services/logger', 'plugins/router'],
         var isBusy = ko.observable(false),
             groupName = ko.observable("Add Group"),
             contactsCount = ko.observable(".."),
-            groupCount = ko.observable(".."),
             groups = ko.observableArray(),
             activate = function () {
-                context.get("/api/address-books/?page=1&size=0").done(function(result){
+                context.get("/api/address-books/?page=1&size=0").done(function (result) {
                     contactsCount(result._count);
                 });
                 return context.get("/address-books/group-options?count=true").done(groups);
             },
             attached = function (view) {
-                context.get("/address-books/group-options?count=true").done(function (result) {
-                    groupCount(result._count);
-                });
 
             },
-            addGroup = function(){
-                groups.push({ group: ko.observable(groupName()), count : ko.observable(0) });
+            addGroup = function () {
+                groups.push({ group: ko.observable(groupName()), count: ko.observable(0) });
                 groupName("Add Group");
             };
 
         var vm = {
             groups: groups,
-            addGroup : addGroup,
-            groupName : groupName,
+            addGroup: addGroup,
+            groupName: groupName,
             contactsCount: contactsCount,
-            groupCount: groupCount,
             isBusy: isBusy,
             activate: activate,
             attached: attached
