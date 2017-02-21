8a801552ed8f0bb0d56d2282cc49d7677735bfe9
Mohammad Abd Razak <mohammad@bespoke.com.my>
2017-02-21 23:46:02 +0800

    Overhaul user profile

--- web/SphApp/viewmodels/customer-home.js
+++ web/SphApp/viewmodels/customer-home.js
@@ -1,19 +1,67 @@
-define(["services/datacontext", "services/logger", "plugins/router", objectbuilders.system, objectbuilders.app, "services/config"],
-    function (context, logger, router, system, app, config) {
-        var activate = function () {
+define(["services/datacontext", "services/logger", "plugins/router", objectbuilders.config, objectbuilders.app, objectbuilders.system],
 
-            return true;
-
-
+function (context, logger, router, config, app, system) {
+    var isBusy = ko.observable(false),
+        errors = ko.observableArray(),
+        entity = ko.observable(),
+        availableCountries = ko.observableArray(),
+        activate = function () {
+            return context.get("/api/address-books/user-profile").done(function (uList) {
+                console.log(uList);
+                if (uList._count == 0) {
+                    entity(new bespoke.Ost_addressBook.domain.AddressBook(system.guid()));
+                    entity().ReferenceNo(config.profile.Email);
+                    entity().CompanyName("");
+                    entity().ContactPerson("");
+                    entity().UserId(config.userName);
+                    entity().ProfilePictureUrl("http://localhost:50230/assets/pages/media/profile/people19.png");
+                    var data = ko.mapping.toJSON(entity);
+                    context.post(data, "/api/address-books/").done(function (result) {
+                        entity().Id(result.id);
+                    });
+                } else {
+                    entity(uList._results[0]);
+                }
+            }).always(function () {
+                context.get("/api/countries/available-country?size=300").done(function (cList) {
+                    availableCountries(cList._results);
+                });
+            });
         },
-            attached = function (view) {
-
-            };
-
-        return {
-            config: config,
-            activate: activate,
-            attached: attached
+        saveProfile = function () {
+            var data = ko.mapping.toJSON(entity);
+            context.put(data, "/api/address-books/" + ko.unwrap(entity().Id) + "").fail(function (response) {
+                var result = response.responseJSON;
+                errors.removeAll();
+                if (response.status === 428) {
+                    logger.error(result.message);
+                }
+                if (response.status === 422 && _(result.rules).isArray()) {
+                    _(result.rules).each(function (v) {
+                        errors(v.ValidationErrors);
+                    });
+                }
+                logger.error("There are errors in your entity, !!!");
+            }).then(function (result) {
+                if (result.success) {
+                    return app.showMessage("Sender details has been successfully saved", "POS Online Shipping Tools", ["OK"]).done(function () {
+                        router.activeItem().activate();
+                    });
+                }
+            });
+        },
+        attached = function (view) {
         };
 
-    });
+    var vm = {
+        errors: errors,
+        entity: entity,
+        config: config,
+        isBusy: isBusy,
+        saveProfile: saveProfile,
+        availableCountries: availableCountries,
+        activate: activate,
+        attached: attached,
+    };
+    return vm;
+});
