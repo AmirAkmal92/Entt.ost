d9931989822df63170c614453519f5e49be2d4e6
Mohammad Abd Razak <mohammad@bespoke.com.my>
2017-02-23 23:20:30 +0800

    Update api key for stagging and production.

--- web/App_Code/TrackTraceController.cs
+++ web/App_Code/TrackTraceController.cs
@@ -15,6 +15,7 @@ public class TrackTraceController : BaseApiController
     public TrackTraceController()
     {
         m_client = new HttpClient { BaseAddress = new Uri(Bespoke.Sph.Domain.ConfigurationManager.GetEnvironmentVariable("SdsBaseUrl") ?? "https://apis.pos.com.my") };
+        //m_client = new HttpClient { BaseAddress = new Uri(Bespoke.Sph.Domain.ConfigurationManager.GetEnvironmentVariable("SdsBaseUrl") ?? "http://stagingsds.pos.com.my") };
     }
 
 
@@ -22,7 +23,8 @@ public class TrackTraceController : BaseApiController
     [Route("{conNote}/{culture?}")]
     public async Task<IHttpActionResult> GetTrackingAsync(string conNote, string culture="En")
     {
-        m_client.DefaultRequestHeaders.Add("X-User-Key", Bespoke.Sph.Domain.ConfigurationManager.GetEnvironmentVariable("SdsTrackTraceKey") ?? "YTk3ZDYyNTgtMzAwMS00ZDQ0LWJjZGUtYTZlYzAxMTY5NDE3");
+        m_client.DefaultRequestHeaders.Add("X-User-Key", Bespoke.Sph.Domain.ConfigurationManager.GetEnvironmentVariable("SdsTrackTraceKey") ?? "YTk3ZDYyNTgtMzAwMS00ZDQ0LWJjZGUtYTZlYzAxMTY5NDE3"); //production server
+        //m_client.DefaultRequestHeaders.Add("X-User-Key", Bespoke.Sph.Domain.ConfigurationManager.GetEnvironmentVariable("SdsTrackTraceKey") ?? "NjY3MmFkMjYtYTgyNy00ODM0LTliMzctYTcxYjAxMjEyZDkx"); //stagging server
         string publishPointingUrl = $"apigateway/as2corporate/api/v2trackntracewebapijson/v1?id={conNote}&Culture={culture}";
     
         var response = await m_client.GetStringAsync(publishPointingUrl );
