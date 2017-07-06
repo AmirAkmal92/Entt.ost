6497819d2560be3a2667a6e67c51f79eee8947f8
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-07-06 18:19:46 +0800

    create paging
    Related Work Items: #6085

--- web/SphApp/viewmodels/pickup-list-daily.js
+++ web/SphApp/viewmodels/pickup-list-daily.js
@@ -1,43 +1,67 @@
-define(["services/datacontext", "services/logger", "plugins/router", "services/chart", objectbuilders.config, "services/_ko.list"],
-function (context, logger, router, chart, config, koList) {
+define(["services/datacontext"], function (context) {
     var isBusy = ko.observable(false),
-        //query = "/api/consigment-requests/?q=Payment.IsPaid:'true'",
-        query = "/api/consigment-requests/", //TODO: create dedicated endpoint for all paid consignments
-        partial = partial || {},
+        page = ko.observable(1),
+        size = ko.observable(20),
+        count = ko.observable(0),
+        availablePageSize = ko.observableArray([10, 20, 30, 50]),
+        hasNextPage = ko.observable(false),
+        hasPreviousPage = ko.observable(false),
+        query = "/api/consigment-requests/paid-all",
         list = ko.observableArray([]),
-        map = function (v) {
-            if (typeof partial.map === "function") {
-                return partial.map(v);
+        firstPage = function () {
+            page(1);
+            count(0);
+            activate();
+        },
+        nextPage = function () {
+            if (hasNextPage()) {
+                page(page() + 1);
+                activate();
             }
-            return v;
+        },
+        previousPage = function () {
+            if (hasPreviousPage()) {
+                page(page() - 1);
+                activate();
+            }
         },
         activate = function () {
-            if (typeof partial.activate === "function") {
-                return partial.activate(list);
-            }
-            return true;
+            url = ko.unwrap(query) + "?page=" + page() + "&size=" + size();
+            return $.ajax({
+                url: url,
+                method: "GET",
+                cache: false
+            })
+            .then(function (lo) {
+                list(lo._results);
+                count(lo._count);
+                hasNextPage(count() > size() * page());
+                hasPreviousPage(page() > 1);
+            });
         },
         attached = function (view) {
-            if (typeof partial.attached === "function") {
-                partial.attached(view);
-            }
+            $('#pager-page-size').change(function () {
+                firstPage();
+            });
         },
         compositionComplete = function () {
-            $('.form-search').hide();
             $('#developers-log-panel-collapse').click();
         };
 
-    var vm = {
+    return {
         query: query,
-        config: config,
         isBusy: isBusy,
-        map: map,
         activate: activate,
         attached: attached,
         compositionComplete: compositionComplete,
-        list: list
+        list: list,
+        page: page,
+        size: size,
+        count: count,
+        availablePageSize: availablePageSize,
+        nextPage: nextPage,
+        previousPage: previousPage,
+        hasNextPage: hasNextPage,
+        hasPreviousPage: hasPreviousPage,
     };
-
-    return vm;
-
 });
