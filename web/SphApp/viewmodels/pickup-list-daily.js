10197b225d746043de114499c4903c36296e57d3
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-07-07 11:50:44 +0800

    OST Daily Transaction Report Searching
    Related Work Items: #6085

--- web/SphApp/viewmodels/pickup-list-daily.js
+++ web/SphApp/viewmodels/pickup-list-daily.js
@@ -4,9 +4,12 @@ define(["services/datacontext"], function (context) {
         size = ko.observable(20),
         count = ko.observable(0),
         availablePageSize = ko.observableArray([10, 20, 30, 50]),
+        dateFrom = ko.observable(moment().format('YYYY-MM-DD')),
+        dateTo = ko.observable(moment().format('YYYY-MM-DD')),
         hasNextPage = ko.observable(false),
         hasPreviousPage = ko.observable(false),
         query = "/api/consigment-requests/paid-all",
+        executedQuery = ko.observable(),
         list = ko.observableArray([]),
         firstPage = function () {
             page(1);
@@ -25,8 +28,27 @@ define(["services/datacontext"], function (context) {
                 activate();
             }
         },
+        reloadPage = function () {
+            window.location.reload(true);
+        },
+        search = function () {
+            if (dateFrom() == null || dateTo() == null
+                || dateFrom() == '' || dateTo() == '') {
+                console.log("Invalid search parameters.");
+            } else {
+                executedQuery("Pickup.DateReady:[" + dateFrom() + " TO " + dateTo() + "]");
+                activate();
+            }
+        },
+        clearSearch = function () {
+            dateFrom(moment().format('YYYY-MM-DD'));
+            dateTo(moment().format('YYYY-MM-DD'));
+            executedQuery(null);
+            activate();
+        },
         activate = function () {
             url = ko.unwrap(query) + "?page=" + page() + "&size=" + size();
+            if (executedQuery() != null) url += "&q=" + executedQuery();
             return $.ajax({
                 url: url,
                 method: "GET",
@@ -42,6 +64,14 @@ define(["services/datacontext"], function (context) {
         attached = function (view) {
             $('#pager-page-size').change(function () {
                 firstPage();
+            });
+            $("#DateFrom").kendoDatePicker({
+                dateInput: true,
+                format: "yyyy-MM-dd"
+            });
+            $("#DateTo").kendoDatePicker({
+                dateInput: true,
+                format: "yyyy-MM-dd"
             });
         },
         compositionComplete = function () {
@@ -59,8 +89,13 @@ define(["services/datacontext"], function (context) {
         size: size,
         count: count,
         availablePageSize: availablePageSize,
+        dateFrom: dateFrom,
+        dateTo: dateTo,
         nextPage: nextPage,
         previousPage: previousPage,
+        reloadPage: reloadPage,
+        search: search,
+        clearSearch: clearSearch,
         hasNextPage: hasNextPage,
         hasPreviousPage: hasPreviousPage,
     };
