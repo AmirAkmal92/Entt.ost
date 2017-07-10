d11d293659ccfde0114c4bce200f80b9e39caa10
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-07-10 19:17:36 +0800

    create project for custom dll to handle RTS pickup to EziSend
    Related Work Items: #6056

---
+++ sources/rts.pickup.ezisend/NotifyEzisendPickup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Bespoke.PosEntt.CustomActions
+{
+    public class NotifyEzisendPickup
+    {
+        public async Task SendNotifyEmail(string emailTo, string emailSubject, string emailMessage,
+            string consignmentNo, DateTime pickupDateTime, string pickupNo)
+        {
+            var emailBody = $@"Hello,
+
+{emailMessage}.
+Your item {consignmentNo} has been successfully picked up at {pickupDateTime} with pickup number {pickupNo}.";
+
+
+            using (var smtp = new SmtpClient())
+            {
+                var mail = new MailMessage("entt.admin@pos.com.my", emailTo)
+                {
+                    Subject = emailSubject,
+                    Body = emailBody,
+                    IsBodyHtml = false
+                };
+                await smtp.SendMailAsync(mail);
+            }
+        }
+    }
+}
