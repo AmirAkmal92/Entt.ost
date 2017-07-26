e778272c8a646aa0d1aeb0008655fb7867c23114
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-07-26 17:47:28 +0800

    Create Controller to Export and Import Contacts to Excel Sheet
    Related Work Items: #6165, #6166

--- web/App_Code/CustomAddressBookController.cs
+++ web/App_Code/CustomAddressBookController.cs
@@ -1,5 +1,11 @@
+using Bespoke.Ost.AddressBooks.Domain;
+using Bespoke.Sph.Domain;
+using Bespoke.Sph.WebApi;
+using Newtonsoft.Json.Linq;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -7,10 +13,6 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Bespoke.Ost.AddressBooks.Domain;
-using Bespoke.Sph.Domain;
-using Bespoke.Sph.WebApi;
-using Newtonsoft.Json.Linq;
 
 namespace web.sph.App_Code
 {
@@ -169,6 +171,168 @@ namespace web.sph.App_Code
             return Ok(new { errors });
         }
 
+        [HttpPost]
+        [Route("import-contacts/{storeId:guid}")]
+        public async Task<IHttpActionResult> ImportContacts2(string storeId)
+        {
+            var errors = new List<object>();
+            var context = new SphDataContext();
+
+            var store = ObjectBuilder.GetObject<IBinaryStore>();
+            var doc = await store.GetContentAsync(storeId);
+            if (null == doc) return Ok(new { success = false, status = $"Cannot find file in {storeId}." });
+
+            var temp = Path.GetTempFileName() + ".xlsx";
+            System.IO.File.WriteAllBytes(temp, doc.Content);
+
+            var file = new FileInfo(temp);
+            var excel = new ExcelPackage(file);
+            var ws = excel.Workbook.Worksheets["Addresses"];
+            if (null == ws) return Ok(new { success = false, status = $"Cannot open Worksheet Addresses in {doc.FileName}." });
+
+            var row = 2;
+            var name = ws.Cells[$"A{row}"].GetValue<string>();
+            var email = ws.Cells[$"B{row}"].GetValue<string>();
+            var postcode = ws.Cells[$"L{row}"].GetValue<string>();
+            var hasRow = !string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(postcode);
+
+            while (hasRow)
+            {
+                var contact = new AddressBook();
+                contact.Id = Guid.NewGuid().ToString();
+                contact.ReferenceNo = email;
+                contact.CompanyName = ws.Cells[$"M{row}"].GetValue<string>();
+                contact.ContactPerson = name;
+                contact.ContactInformation.Email = email;
+                contact.ContactInformation.ContactNumber = ws.Cells[$"C{row}"].GetValue<string>();
+                contact.ContactInformation.AlternativeContactNumber = ws.Cells[$"D{row}"].GetValue<string>();
+                contact.Address.Address1 = ws.Cells[$"E{row}"].GetValue<string>();
+                contact.Address.Address2 = ws.Cells[$"F{row}"].GetValue<string>();
+                contact.Address.Address3 = ws.Cells[$"G{row}"].GetValue<string>();
+                contact.Address.Address4 = ws.Cells[$"H{row}"].GetValue<string>();
+                contact.Address.City = ws.Cells[$"I{row}"].GetValue<string>();
+                contact.Address.State = ws.Cells[$"J{row}"].GetValue<string>();
+                contact.Address.Country = ws.Cells[$"K{row}"].GetValue<string>();
+                contact.Address.Postcode = postcode;
+                contact.UserId = User.Identity.Name;
+
+                row++;
+                name = ws.Cells[$"A{row}"].GetValue<string>();
+                email = ws.Cells[$"B{row}"].GetValue<string>();
+                postcode = ws.Cells[$"L{row}"].GetValue<string>();
+                hasRow = !string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(postcode);
+
+                try
+                {
+                    using (var session = context.OpenSession())
+                    {
+                        session.Attach(contact);
+                        await session.SubmitChanges("ImportContacts", new Dictionary<string, object> { { "username", User.Identity.Name } });
+                    }
+                }
+                catch (Exception e)
+                {
+                    errors.Add(new { message = e.Message, contact = contact });
+                }
+            }
+
+            await store.DeleteAsync(storeId);
+
+            if (errors.Count > 0)
+                return Ok(new { success = false, status = "Some errors happened." });
+
+            return Ok(new { success = true, status = "OK" });
+        }
+
+        [HttpGet]
+        [Route("export-contacts")]
+        public async Task<IHttpActionResult> ExportContacts()
+        {
+            var temp = Path.GetTempFileName() + ".xlsx";
+            System.IO.File.Copy(System.Web.HttpContext.Current.Server.MapPath("~/Content/Files/address_format_template.xlsx"), temp, true);
+
+            var file = new FileInfo(temp);
+            var excel = new ExcelPackage(file);
+            var ws = excel.Workbook.Worksheets["Addresses"];
+            if (null == ws) return Ok(new { success = false, status = $"Cannot open Worksheet Addresses" });
+
+            var contacts = new List<AddressBook>();
+            var rows = int.MaxValue;
+            var page = 0;
+            const int size = 50;
+            var repos = ObjectBuilder.GetObject<IReadonlyRepository<AddressBook>>();
+            var query = $@"{{
+   ""query"": {{
+       ""term"": {{
+          ""CreatedBy"": {{
+             ""value"": ""{User.Identity.Name}""
+          }}
+       }}
+   }}
+}}";
+            while (contacts.Count < rows)
+            {
+                page++;
+
+                var queryString = $"from={size * (page - 1)}&size={size}";
+
+                var response = await repos.SearchAsync(query, queryString);
+                var json = JObject.Parse(response);
+                var addresses = from f in json.SelectToken("$.hits.hits")
+                                select f.SelectToken("_source").ToString().DeserializeFromJson<AddressBook>();
+
+                contacts.AddRange(addresses);
+                rows = json.SelectToken("$.hits.total").Value<int>();
+            }
+
+            var existingLinesInTemplate = 2;
+            var row = 2;
+
+            //Empty existing lines in template
+            if (contacts.Count < 2)
+            {
+                for (var i = 0; i < existingLinesInTemplate; i++)
+                {
+                    ws.Cells[row + i, 1].Value = string.Empty;
+                    ws.Cells[row + i, 2].Value = string.Empty;
+                    ws.Cells[row + i, 3].Value = string.Empty;
+                    ws.Cells[row + i, 4].Value = string.Empty;
+                    ws.Cells[row + i, 5].Value = string.Empty;
+                    ws.Cells[row + i, 6].Value = string.Empty;
+                    ws.Cells[row + i, 7].Value = string.Empty;
+                    ws.Cells[row + i, 8].Value = string.Empty;
+                    ws.Cells[row + i, 9].Value = string.Empty;
+                    ws.Cells[row + i, 10].Value = string.Empty;
+                    ws.Cells[row + i, 11].Value = string.Empty;
+                    ws.Cells[row + i, 12].Value = string.Empty;
+                    ws.Cells[row + i, 13].Value = string.Empty;
+                }
+            }
+
+            foreach (var contact in contacts)
+            {
+                ws.Cells[row, 1].Value = contact.ContactPerson;
+                ws.Cells[row, 2].Value = contact.ContactInformation.Email;
+                ws.Cells[row, 3].Value = contact.ContactInformation.ContactNumber;
+                ws.Cells[row, 4].Value = contact.ContactInformation.AlternativeContactNumber;
+                ws.Cells[row, 5].Value = contact.Address.Address1;
+                ws.Cells[row, 6].Value = contact.Address.Address2;
+                ws.Cells[row, 7].Value = contact.Address.Address3;
+                ws.Cells[row, 8].Value = contact.Address.Address4;
+                ws.Cells[row, 9].Value = contact.Address.City;
+                ws.Cells[row, 10].Value = contact.Address.State;
+                ws.Cells[row, 11].Value = contact.Address.Country;
+                ws.Cells[row, 12].Value = contact.Address.Postcode;
+                ws.Cells[row, 13].Value = contact.CompanyName;
+                row++;
+            }
+
+            excel.Save();
+            excel.Dispose();
+
+            return Json(new { success = true, status = "OK", path = Path.GetFileName(temp) });
+        }
+
         [HttpGet]
         [Route("group-options")]
         public async Task<IHttpActionResult> GetGroupOptions([FromUri(Name = "count")]bool withCount = false)
@@ -278,7 +442,7 @@ namespace web.sph.App_Code
                 csv.Append(@"Email,");
             if (contactNumber)
                 csv.Append(@"Mobile Number,");
-               csv.Append(@"Country Code,");
+            csv.Append(@"Country Code,");
             if (altContactNumber)
                 csv.Append(@"Alternative Contact Number,");
             if (address1)
@@ -297,7 +461,7 @@ namespace web.sph.App_Code
                 csv.Append(@"Country,");
             if (postcode)
                 csv.Append(@"Postcode,");
-               csv.Append(@"Designation,");
+            csv.Append(@"Designation,");
             if (companyName)
                 csv.Append(@"Company");
 
@@ -305,14 +469,14 @@ namespace web.sph.App_Code
 
             foreach (var adr in list)
             {
-               
+
                 if (contactPerson)
                     csv.Append($@"{adr.ContactPerson},");
                 if (email)
                     csv.Append($@"{adr.ContactInformation.Email},");
                 if (contactNumber)
                     csv.Append($@"{adr.ContactInformation.ContactNumber},");
-                    csv.Append($@"60-Malaysia,");
+                csv.Append($@"60-Malaysia,");
                 if (altContactNumber)
                     csv.Append($@"{adr.ContactInformation.AlternativeContactNumber},");
                 if (address1)
@@ -331,7 +495,7 @@ namespace web.sph.App_Code
                     csv.Append($@"{adr.Address.Country},");
                 if (postcode)
                     csv.Append($@"{adr.Address.Postcode},");
-                    csv.Append($@"No,");
+                csv.Append($@"No,");
                 if (companyName)
                     csv.Append($@"{adr.CompanyName}");
 
---
+++ web/App_Code/PrintExcelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.sph.App_Code
+{
+    [RoutePrefix("print-excel")]
+    public partial class PrintExcelController : Controller
+    {
+
+        [Route("file-path/{filePath}/file-name/{fileName}")]
+        public FilePathResult GetExcelFile(string filePath, string fileName)
+        {
+            var path = Path.Combine(Path.GetTempPath(), filePath);
+            return File(path, MimeMapping.GetMimeMapping(filePath), $"{fileName}_{DateTime.Now:yyyyMMdd-HHmmss}.xlsx");
+        }
+
+    }
+}
