f22c97c08d0920db2b81e7d536826e1e9d923388
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-07-26 17:51:04 +0800

    clean donwload dialogs
    Related Work Items: #6166

--- web/SphApp/viewmodels/export.addresses.dialog.js
+++ web/SphApp/viewmodels/export.addresses.dialog.js
@@ -1,23 +1,6 @@
 define(["plugins/dialog"],
     function (dialog) {
-        var options = ko.observable({
-                companyName: ko.observable(false),
-                contactPerson: ko.observable(false),
-                address1: ko.observable(false),
-                address2: ko.observable(false),
-                address3: ko.observable(false),
-                address4: ko.observable(false),
-                postcode: ko.observable(false),
-                city: ko.observable(false),
-                state: ko.observable(false),
-                country: ko.observable(false),
-                contactNumber: ko.observable(false),
-                altContactNumber: ko.observable(false),
-                email: ko.observable(false),
-                gpsLocation: ko.observable(false),
-                addressGroup: ko.observable(false)
-            }),
-            okClick = function (data, ev) {
+        var okClick = function (data, ev) {
                 dialog.close(this, "OK");
             },
         cancelClick = function () {
@@ -25,7 +8,6 @@ define(["plugins/dialog"],
         };
 
         var vm = {
-            options: options,
             okClick: okClick,
             cancelClick: cancelClick
         };
