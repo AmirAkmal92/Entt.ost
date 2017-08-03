d921d50ed066f072e9ea22ca11cf0ef47e4f9aad
zakirin zahari <zakirin@bespoke.com.my>
2017-08-03 11:29:22 +0800

    Implement Purchase Topup Logic
    Related Work Items: #6188

--- web/App_Code/OstPaymentController.cs
+++ web/App_Code/OstPaymentController.cs
@@ -1,4 +1,6 @@
 ﻿using Bespoke.Ost.ConsigmentRequests.Domain;
+using Bespoke.Ost.GeneralLedgers.Domain;
+using Bespoke.Ost.UserDetails.Domain;
 using Bespoke.Ost.Wallets.Domain;
 using Bespoke.Sph.Domain;
 using System;
@@ -136,30 +138,42 @@ namespace web.sph.App_Code
         [Route("ps-request-prepaid/{id}")]
         public async Task<ActionResult> PsRequestPrepaid(string id)
         {
-            LoadData<Wallet> lo = await GetWalletCode(id);
+            var context = new SphDataContext();
+            LoadData<Wallet> lo = await GetWallet(id);
             if (null == lo.Source)
             {
                 Response.StatusCode = (int)HttpStatusCode.NotFound;
                 return Json(new { success = false, status = "ERROR", message = $"Cannot find WalletCode with Id: {id}." }, JsonRequestBehavior.AllowGet);
             }
 
-            var item = lo.Source;
-            var model = new PaymentSwitchRequestModel();
+            var generalLedger = new GeneralLedger();
+            generalLedger.Id = Guid.NewGuid().ToString();
 
+            var wallet = lo.Source;
             decimal noGstPrice = 0;
             decimal gstPrice = 0;
             decimal totalPrice = 0;
-            noGstPrice = Convert.ToDecimal(item.TotalValue);
+            noGstPrice = Convert.ToDecimal(wallet.TotalValue);
             gstPrice = noGstPrice * Convert.ToDecimal(0.06);
             totalPrice = noGstPrice + gstPrice;
 
             // required by payment gateway
-            model.TransactionId = GenerateCustomRefNo(item);
+            var model = new PaymentSwitchRequestModel();
+            model.TransactionId = GenerateCustomRefNo(generalLedger);
             model.TransactionAmount = noGstPrice;
             model.TransactionGST = gstPrice;
             model.PurchaseDate = DateTime.Now;
-            model.Description = $"{m_applicationName} purchase by {User.Identity.Name} for RM{totalPrice}";
-            model.CallbackUrl = $"{m_baseUrl}/ost-payment/ps-response-prepaid"; //TODO TEMP ONLY
+            model.Description = $"{m_applicationName} purchase topup by {User.Identity.Name} for RM{totalPrice} with Id: ({wallet.WalletCode})";
+            model.CallbackUrl = $"{m_baseUrl}/ost-payment/ps-response-prepaid";
+
+            //insert empty transaction into GL with reference no and type
+            generalLedger.ReferenceNo = model.TransactionId;
+            generalLedger.Type = "Prepaid";
+            using (var session = context.OpenSession())
+            {
+                session.Attach(generalLedger);
+                await session.SubmitChanges("Default");
+            }
 
             var rijndaelKey = new RijndaelEnhanced(m_paymentGatewayEncryptionKey);
             var dataToEncrypt = string.Format("{0}|{1}|{2}|{3}|{4}", model.TransactionId, model.TransactionAmount, model.TransactionGST, model.PurchaseDate.ToString("MM/dd/yyyy hh:mm:ss"), model.Description);
@@ -176,46 +190,92 @@ namespace web.sph.App_Code
         [Route("ps-response-prepaid")]
         public async Task<ActionResult> PsResponsePrepaid()
         {
+            var context = new SphDataContext();
+            //TODO: Clean-up unused GLs
+            //1. get all GLs owned by current user with (Type == "Prepaid") AND (UserId == null)        
+            //2. delete all found GLs
+
             var encryptedData = Request.Form["data"];
 
             if (string.IsNullOrEmpty(encryptedData))
             {
-                return Redirect($"{m_baseUrl}/ost");
+                return Redirect($"{m_baseUrl}/ost#wallet-list-all");
             }
 
             var rijndaelKey = new RijndaelEnhanced(m_paymentGatewayEncryptionKey);
             var decryptedData = rijndaelKey.Decrypt(encryptedData);
 
             var decryptedDataArr = decryptedData.Split(new char[] { '|' });
+
             var model = new PaymentSwitchResponseModel();
-
-            //TODO: But for now just print at console
             model.TransactionId = decryptedDataArr[0];
-            Console.WriteLine(model.TransactionId);
-
             model.TransactionAmount = decryptedDataArr[1];
-            Console.WriteLine(model.TransactionAmount);
-
             model.TransactionGST = decryptedDataArr[2];
-            Console.WriteLine(model.TransactionGST);
-
             model.ServiceFee = decryptedDataArr[3];
-            Console.WriteLine(model.ServiceFee);
-
             model.ServiceGST = decryptedDataArr[4];
-            Console.WriteLine(model.ServiceGST);
-
             model.TotalAmount = decryptedDataArr[5];
-            Console.WriteLine(model.TotalAmount);
-
             model.Status = decryptedDataArr[6];
-            Console.WriteLine(model.Status);
-
             model.ErrorMessage = decryptedDataArr[7];
             Console.WriteLine(model.ErrorMessage);
 
+            //get wallet to determine wallet Id as GL correspondingId
+            LoadData<Wallet> loWallet = await GetWalletByTotalValue(model.TransactionAmount);
+            if (null == loWallet.Source)
+            {
+                Response.StatusCode = (int)HttpStatusCode.NotFound;
+                return Json(new { success = false, status = "ERROR", message = $"Cannot find TransactionAmount with Id: {model.TransactionAmount}." }, JsonRequestBehavior.AllowGet);
+            }
+            var wallet = loWallet.Source;
+
+            //get userdetail to update AccountBalance
+            LoadData<UserDetail> loUserDetail = await GetUserDetail(User.Identity.Name);
+            if (null == loUserDetail.Source)
+            {
+                Response.StatusCode = (int)HttpStatusCode.NotFound;
+                return Json(new { success = false, status = "ERROR", message = $"Cannot find UserId with Id: {User.Identity.Name}." }, JsonRequestBehavior.AllowGet);
+            }
+            var userDetail = loUserDetail.Source;
+
+            //verify reference no from GL (get by reference no)
+            LoadData<GeneralLedger> loGeneralLedger = await GetGeneralLedger(model.TransactionId);
+            if (null == loGeneralLedger.Source)
+            {
+                Response.StatusCode = (int)HttpStatusCode.NotFound;
+                return Json(new { success = false, status = "ERROR", message = $"Cannot find ReferenceNo with Id: {model.TransactionId}." }, JsonRequestBehavior.AllowGet);
+            }
+            var generalLedger = loGeneralLedger.Source;
+
+            if (model.Status.Equals("1"))
+            {
+                //update GeneralLedger
+                generalLedger.ReferenceNo = model.TransactionId;
+                generalLedger.UserId = User.Identity.Name;
+                generalLedger.Type = "Prepaid";
+                generalLedger.CorrespondingId = wallet.Id;
+                generalLedger.Description = $"{m_applicationName} purchase topup by {User.Identity.Name} for RM{model.TransactionAmount}";
+                generalLedger.Amount = Convert.ToDecimal(model.TransactionAmount);
+                generalLedger.ExistingAmount = Convert.ToDecimal(userDetail.AccountBalance);
+                generalLedger.NewAmount = Convert.ToDecimal(model.TransactionAmount) + Convert.ToDecimal(userDetail.AccountBalance);
+
+                //update UserDetail
+                userDetail.AccountBalance = generalLedger.NewAmount;
+                
+                using (var session = context.OpenSession())
+                {
+                    session.Attach(generalLedger);
+                    session.Attach(userDetail);
+                    await session.SubmitChanges("Default");
+                }
+                await Task.Delay(1500);
+                return Redirect($"{m_baseUrl}/ost#wallet-history");
+            }
+            else
+            {
+                //fall through
+            }
+
             await Task.Delay(1500);
-            return Redirect($"{m_baseUrl}/ost");
+            return Redirect($"{m_baseUrl}/ost#wallet-list-all");
         }
 
         private static async Task<LoadData<ConsigmentRequest>> GetConsigmentRequest(string id)
@@ -228,13 +288,47 @@ namespace web.sph.App_Code
             return lo;
         }
 
-        private static async Task<LoadData<Wallet>> GetWalletCode(string id)
+        private static async Task<LoadData<Wallet>> GetWallet(string id)
         {
             var repos = ObjectBuilder.GetObject<IReadonlyRepository<Wallet>>();
 
             var lo = await repos.LoadOneAsync(id);
             if (null == lo.Source)
                 lo = await repos.LoadOneAsync("WalletCode", id);
+            return lo;
+        }
+
+        private static async Task<LoadData<Wallet>> GetWalletByTotalValue(string totalValue)
+        {
+            var repos = ObjectBuilder.GetObject<IReadonlyRepository<Wallet>>();
+            var lo = await repos.LoadOneAsync("TotalValue", totalValue);
+            return lo;
+        }
+
+        private static async Task<LoadData<GeneralLedger>> GetGeneralLedger(string id)
+        {
+            var repos = ObjectBuilder.GetObject<IReadonlyRepository<GeneralLedger>>();
+
+            var lo = await repos.LoadOneAsync(id);
+            if (null == lo.Source)
+                lo = await repos.LoadOneAsync("ReferenceNo", id);
+            return lo;
+        }
+
+        private static async Task<LoadData<GeneralLedger>> GetGeneralLedgerByCreatedBy(string createdBy)
+        {
+            var repos = ObjectBuilder.GetObject<IReadonlyRepository<GeneralLedger>>();
+            var lo = await repos.LoadOneAsync("CreatedBy", createdBy);
+            return lo;
+        }
+
+        private static async Task<LoadData<UserDetail>> GetUserDetail(string id)
+        {
+            var repos = ObjectBuilder.GetObject<IReadonlyRepository<UserDetail>>();
+
+            var lo = await repos.LoadOneAsync(id);
+            if (null == lo.Source)
+                lo = await repos.LoadOneAsync("UserId", id);
             return lo;
         }
 
@@ -248,7 +342,7 @@ namespace web.sph.App_Code
             }
         }
 
-        private string GenerateCustomRefNo(Wallet item)
+        private string GenerateCustomRefNo(GeneralLedger item)
         {
             var referenceNo = new StringBuilder();
             referenceNo.Append($"{m_applicationName.ToUpper()}-");
