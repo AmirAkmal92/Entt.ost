0076b52f8ddf28146908215d29658d4000a7e461
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-08-15 19:29:22 +0800

    Create button to download tallysheet at Pickups

--- web/SphApp/partial/consignment-requests-pickedup.js
+++ web/SphApp/partial/consignment-requests-pickedup.js
@@ -1,4 +1,4 @@
-﻿define([], function () {
+﻿define(["services/datacontext"], function (context) {
     var activate = function (list) {
 
         var tcs = new $.Deferred();
@@ -15,6 +15,41 @@
     },
     map = function (v) {
         return v;
+    },
+    exportTallysheetPickup = function (consignment) {
+        require(['viewmodels/export.tallysheet.dialog', 'durandal/app'], function (dialog, app2) {
+            dialog.moduleType("pickups");
+            app2.showDialog(dialog)
+                .done(function (result) {
+                    if (!result) return;
+                    if (result === "OK") {
+                        generateTallysheetFromConsignments(consignment.Id);
+                    }
+                });
+        });
+    },
+    generateTallysheetFromConsignments = function (id) {
+        context.put({}, "/consignment-request/export-tallysheet/" + id)
+            .fail(function (response) {
+                if (response.status === 428) {
+                    // out of date conflict
+                    logger.error(result.message);
+                }
+                if (response.status === 422 && _(result.rules).isArray()) {
+                    _(result.rules).each(function (v) {
+                        errors(v.ValidationErrors);
+                    });
+                }
+                logger.error("There are errors in your entity, !!!");
+            })
+            .then(function (result) {
+                if (result.status === "OK") {
+                    if (result.success) {
+                        var fileName = "Pickups_Tallysheet";
+                        window.open("/print-excel/file-path/" + result.path + "/file-name/" + fileName);
+                    }
+                }
+            });
     },
     showParcelsTrackTrace = function (consignmentRequest) {
         console.log(consignmentRequest);
@@ -50,6 +85,7 @@
         activate: activate,
         attached: attached,
         map: map,
+        exportTallysheetPickup: exportTallysheetPickup,
         showParcelsTrackTrace: showParcelsTrackTrace,
         showParcelTrackTrace: showParcelTrackTrace
     };
