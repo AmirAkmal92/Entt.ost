34542ae97d1c52ffae70bf54b443634310919059
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-11-23 10:56:26 +0800

    create new empty console app to for fetching pickup status from rts

---
+++ sources/ezisend.fetch.pickup/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ezisend.fetch.pickup
+{
+    class Program
+    {
+        private string m_ostBaseUrl;
+        private string m_ostAdminToken;
+
+        public Program()
+        {
+            m_ostBaseUrl = ConfigurationManager.GetEnvironmentVariable("BaseUrl") ?? "http://localhost:50230";
+            m_ostAdminToken = ConfigurationManager.GetEnvironmentVariable("AdminToken") ?? "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiYWRtaW4iLCJyb2xlcyI6WyJhZG1pbmlzdHJhdG9ycyIsImNhbl9lZGl0X2VudGl0eSIsImNhbl9lZGl0X3dvcmtmbG93IiwiZGV2ZWxvcGVycyJdLCJlbWFpbCI6ImFkbWluQHlvdXJjb21wYW55LmNvbSIsInN1YiI6IjYzNjIwNDQ2NTgyNzk2MDA0NDYwOGNjMzdjIiwibmJmIjoxNTAwNDU5MzgzLCJpYXQiOjE0ODQ4MjA5ODMsImV4cCI6MTUxNDY3ODQwMCwiYXVkIjoiT3N0In0.qIA-b-0XTI_GpgMCGJC1yAAtw04UoPaNYoxMSXeBrPk";
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var program = new Program();
+                program.RunAsync().Wait();
+                Console.WriteLine("Done ......");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        public async Task RunAsync()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("ezisend.fetch.pickup");
+            Console.ReadLine();
+        }
+    }
+}
