fc42b354035b0ca73e523be9a1c862590a24c135
Nazrul Hisham <nazrulhisham@bespoke.com.my>
2017-11-29 10:39:58 +0800

    refactor & clean ezisend utility projects

--- sources/ezisend.clear.expired.pickup/Program.cs
+++ sources/ezisend.clear.expired.pickup/Program.cs
@@ -1,32 +1,31 @@
-﻿using Bespoke.Ost.ConsigmentRequests.Domain;
-using Bespoke.Sph.Domain;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Bespoke.Ost.ConsigmentRequests.Domain;
+using Bespoke.Sph.Domain;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ezisend.clear.expired.pickup
 {
     class Program
     {
-        private HttpClient m_ostClient;
-        private string m_ostBaseUrl;
-        private string m_ostAdminToken;
+        private readonly HttpClient m_ostClient;
 
         public Program()
         {
-            m_ostBaseUrl = ConfigurationManager.GetEnvironmentVariable("BaseUrl") ?? "http://localhost:50230";
-            m_ostAdminToken = ConfigurationManager.GetEnvironmentVariable("AdminToken") ?? "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiYWRtaW4iLCJyb2xlcyI6WyJhZG1pbmlzdHJhdG9ycyIsImNhbl9lZGl0X2VudGl0eSIsImNhbl9lZGl0X3dvcmtmbG93IiwiZGV2ZWxvcGVycyJdLCJlbWFpbCI6ImFkbWluQHlvdXJjb21wYW55LmNvbSIsInN1YiI6IjYzNjIwNDQ2NTgyNzk2MDA0NDYwOGNjMzdjIiwibmJmIjoxNTAwNDU5MzgzLCJpYXQiOjE0ODQ4MjA5ODMsImV4cCI6MTUxNDY3ODQwMCwiYXVkIjoiT3N0In0.qIA-b-0XTI_GpgMCGJC1yAAtw04UoPaNYoxMSXeBrPk";
+            var ostBaseUrl = ConfigurationManager.GetEnvironmentVariable("BaseUrl") ?? "http://localhost:50230";
+            var ostAdminToken = ConfigurationManager.GetEnvironmentVariable("AdminToken") ?? "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiYWRtaW4iLCJyb2xlcyI6WyJhZG1pbmlzdHJhdG9ycyIsImNhbl9lZGl0X2VudGl0eSIsImNhbl9lZGl0X3dvcmtmbG93IiwiZGV2ZWxvcGVycyJdLCJlbWFpbCI6ImFkbWluQHlvdXJjb21wYW55LmNvbSIsInN1YiI6IjYzNjIwNDQ2NTgyNzk2MDA0NDYwOGNjMzdjIiwibmJmIjoxNTAwNDU5MzgzLCJpYXQiOjE0ODQ4MjA5ODMsImV4cCI6MTUxNDY3ODQwMCwiYXVkIjoiT3N0In0.qIA-b-0XTI_GpgMCGJC1yAAtw04UoPaNYoxMSXeBrPk";
 
-            m_ostClient = new HttpClient { BaseAddress = new Uri(m_ostBaseUrl) };
-            m_ostClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", m_ostAdminToken);
+            m_ostClient = new HttpClient { BaseAddress = new Uri(ostBaseUrl) };
+            m_ostClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ostAdminToken);
         }
 
-        static void Main(string[] args)
+        private static void Main()
         {
             try
             {
@@ -47,24 +46,21 @@ namespace ezisend.clear.expired.pickup
 
             foreach (var consigmentRequest in consigmentRequests)
             {
-                if (!string.IsNullOrEmpty(consigmentRequest.Pickup.Number)
-                    && !consigmentRequest.Pickup.IsPickedUp
-                    && consigmentRequest.Pickup.DateReady < now
-                    && consigmentRequest.Pickup.DateClose < now)
-                {
-                    Console.WriteLine($"Renewing: {consigmentRequest.Id}");
-                    var success = await RenewPickup(consigmentRequest);
-                    Console.WriteLine();
-                    if (!success) break;
-                }
+                if (string.IsNullOrEmpty(consigmentRequest.Pickup.Number) || consigmentRequest.Pickup.IsPickedUp ||
+                    consigmentRequest.Pickup.DateReady >= now || consigmentRequest.Pickup.DateClose >= now) continue;
+
+                Console.WriteLine($"Renewing: {consigmentRequest.Id}");
+                var success = await RenewPickup(consigmentRequest);
+                Console.WriteLine();
+
+                if (!success) break;
             }
         }
 
         private async Task<bool> RenewPickup(ConsigmentRequest consigmentRequest)
         {
-            var success = true;
             var json = JsonConvert.SerializeObject(consigmentRequest);
-            var content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
             var requestUri = $"/consignment-request/renew-pickup/{consigmentRequest.Id}";
             var response = await m_ostClient.PutAsync(requestUri, content);
 
@@ -80,17 +76,16 @@ namespace ezisend.clear.expired.pickup
             var consigmentRequestId = JObject.Parse(output).SelectToken("$.id").Value<string>();
             Console.WriteLine($"Renewed: {consigmentRequestId}");
 
-            return success;
+            return true;
         }
 
         private async Task<List<ConsigmentRequest>> GetConsignmentRequest()
         {
             var consigmentRequests = new List<ConsigmentRequest>();
 
-            bool more = true;
-            int count = 0;
-            int page = 1;
-            int size = 20;
+            var more = true;
+            var page = 1;
+            var size = 20;
 
             while (more)
             {
@@ -103,14 +98,10 @@ namespace ezisend.clear.expired.pickup
 
                 var output = await response.Content.ReadAsStringAsync();
                 var json = JObject.Parse(output).SelectToken("_results");
-                foreach (var jtok in json)
-                {
-                    var consigmentRequest = jtok.ToJson().DeserializeFromJson<ConsigmentRequest>();
-                    consigmentRequests.Add(consigmentRequest);
-                }
+                consigmentRequests.AddRange(json.Select(jtok => jtok.ToJson().DeserializeFromJson<ConsigmentRequest>()));
                 Console.WriteLine($"Consigment Request count: {consigmentRequests.Count} .....");
 
-                count = JObject.Parse(output).SelectToken("_count").Value<int>();
+                var count = JObject.Parse(output).SelectToken("_count").Value<int>();
                 page = JObject.Parse(output).SelectToken("_page").Value<int>();
                 size = JObject.Parse(output).SelectToken("_size").Value<int>();
                 if ((page * size) >= count)
--- sources/ezisend.fetch.pickup/Program.cs
+++ sources/ezisend.fetch.pickup/Program.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -14,30 +15,26 @@ namespace ezisend.fetch.pickup
 {
     class Program
     {
-        private bool m_doPosting;
-        private HttpClient m_ostClient;
-        private HttpClient m_rtsClient;
-        private string m_ostBaseUrl;
-        private string m_ostAdminToken;
-        private string m_rtsBaseUrl;
-        private string m_rtsAdminToken;
+        private readonly bool m_doPosting;
+        private readonly HttpClient m_ostClient;
+        private readonly HttpClient m_rtsClient;
 
         public Program()
         {
             m_doPosting = true;//posting switch
-            m_ostBaseUrl = ConfigurationManager.GetEnvironmentVariable("BaseUrl") ?? "https://ezisend.poslaju.com.my";
-            m_ostAdminToken = ConfigurationManager.GetEnvironmentVariable("AdminToken") ?? "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiYWRtaW4iLCJyb2xlcyI6WyJhZG1pbmlzdHJhdG9ycyIsImNhbl9lZGl0X2VudGl0eSIsImNhbl9lZGl0X3dvcmtmbG93IiwiZGV2ZWxvcGVycyJdLCJlbWFpbCI6ImFkbWluQHlvdXJjb21wYW55LmNvbSIsInN1YiI6IjYzNjI1ODg3Nzc4NjYwMDg3NTVmMTgxMDQ0IiwibmJmIjoxNTA2MTU5Nzc5LCJpYXQiOjE0OTAyNjIxNzksImV4cCI6MTc2NzEzOTIwMCwiYXVkIjoiT3N0In0.DBMfLcyIdXsOl65p34hA7MOhUFimpGJYXGRn4-alfBI";
-            m_rtsBaseUrl = ConfigurationManager.GetEnvironmentVariable("RtsBaseUrl") ?? "http://rx.pos.com.my";
-            m_rtsAdminToken = ConfigurationManager.GetEnvironmentVariable("RtsAdminToken") ?? "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiYWRtaW4iLCJyb2xlcyI6WyJhZG1pbmlzdHJhdG9ycyIsImNhbl9lZGl0X2VudGl0eSIsImNhbl9lZGl0X3dvcmtmbG93IiwiZGV2ZWxvcGVycyJdLCJlbWFpbCI6ImFkbWluQHBvcy5jb20ubXkiLCJzdWIiOiI2MzYzODk1NjI1NzE1OTY2NTFjNDkwNzRjZSIsIm5iZiI6MTUxOTIyODI1NywiaWF0IjoxNTAzMzMwNjU3LCJleHAiOjE2MDkzNzI4MDAsImF1ZCI6IlBvc0VudHQifQ.-LxvJ8J4bS1xogV3gIoBtMkqlr1h1zP71FUhFA9MuxE";
+            var ostBaseUrl = ConfigurationManager.GetEnvironmentVariable("BaseUrl") ?? "https://ezisend.poslaju.com.my";
+            var ostAdminToken = ConfigurationManager.GetEnvironmentVariable("AdminToken") ?? "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiYWRtaW4iLCJyb2xlcyI6WyJhZG1pbmlzdHJhdG9ycyIsImNhbl9lZGl0X2VudGl0eSIsImNhbl9lZGl0X3dvcmtmbG93IiwiZGV2ZWxvcGVycyJdLCJlbWFpbCI6ImFkbWluQHlvdXJjb21wYW55LmNvbSIsInN1YiI6IjYzNjI1ODg3Nzc4NjYwMDg3NTVmMTgxMDQ0IiwibmJmIjoxNTA2MTU5Nzc5LCJpYXQiOjE0OTAyNjIxNzksImV4cCI6MTc2NzEzOTIwMCwiYXVkIjoiT3N0In0.DBMfLcyIdXsOl65p34hA7MOhUFimpGJYXGRn4-alfBI";
+            var rtsBaseUrl = ConfigurationManager.GetEnvironmentVariable("RtsBaseUrl") ?? "http://rx.pos.com.my";
+            var rtsAdminToken = ConfigurationManager.GetEnvironmentVariable("RtsAdminToken") ?? "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiYWRtaW4iLCJyb2xlcyI6WyJhZG1pbmlzdHJhdG9ycyIsImNhbl9lZGl0X2VudGl0eSIsImNhbl9lZGl0X3dvcmtmbG93IiwiZGV2ZWxvcGVycyJdLCJlbWFpbCI6ImFkbWluQHBvcy5jb20ubXkiLCJzdWIiOiI2MzYzODk1NjI1NzE1OTY2NTFjNDkwNzRjZSIsIm5iZiI6MTUxOTIyODI1NywiaWF0IjoxNTAzMzMwNjU3LCJleHAiOjE2MDkzNzI4MDAsImF1ZCI6IlBvc0VudHQifQ.-LxvJ8J4bS1xogV3gIoBtMkqlr1h1zP71FUhFA9MuxE";
 
-            m_ostClient = new HttpClient { BaseAddress = new Uri(m_ostBaseUrl) };
-            m_ostClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", m_ostAdminToken);
+            m_ostClient = new HttpClient { BaseAddress = new Uri(ostBaseUrl) };
+            m_ostClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ostAdminToken);
 
-            m_rtsClient = new HttpClient { BaseAddress = new Uri(m_rtsBaseUrl) };
-            m_rtsClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", m_rtsAdminToken);
+            m_rtsClient = new HttpClient { BaseAddress = new Uri(rtsBaseUrl) };
+            m_rtsClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", rtsAdminToken);
         }
 
-        static void Main(string[] args)
+        private static void Main()
         {
             try
             {
@@ -56,14 +53,7 @@ namespace ezisend.fetch.pickup
             var consigmentRequests = await GetConsignmentRequest();
             foreach (var consigmentRequest in consigmentRequests)
             {
-                var connotes = new List<string>();
-                foreach (var consignment in consigmentRequest.Consignments)
-                {
-                    if (!string.IsNullOrEmpty(consignment.ConNote))
-                    {
-                        connotes.Add(consignment.ConNote);
-                    }
-                }
+                var connotes = (from consignment in consigmentRequest.Consignments where !string.IsNullOrEmpty(consignment.ConNote) select consignment.ConNote).ToList();
 
                 var stringOfConnotes = JsonConvert.SerializeObject(connotes);
                 Console.WriteLine(string.Empty);
@@ -72,14 +62,12 @@ namespace ezisend.fetch.pickup
                 Console.WriteLine($"Connote(s) ({connotes.Count})/({consigmentRequest.Consignments.Count}): {stringOfConnotes}");
 
                 var pickups = await GetPickupEvents(stringOfConnotes);
-                if (pickups.Count > 0)
+                if (pickups.Count <= 0) continue;
+                Console.WriteLine($"Pickup(s) ({pickups.Count}):");
+                foreach (var pickup in pickups)
                 {
-                    Console.WriteLine($"Pickup(s) ({pickups.Count}):");
-                    foreach (var pickup in pickups)
-                    {
-                        var rtsPickupFormat = CreateRtsPickupFormatFromPickup(pickup);
-                        await PostRtsPickupFormat(rtsPickupFormat, m_doPosting);
-                    }
+                    var rtsPickupFormat = CreateRtsPickupFormatFromPickup(pickup);
+                    await PostRtsPickupFormat(rtsPickupFormat, m_doPosting);
                 }
             }
         }
@@ -87,10 +75,9 @@ namespace ezisend.fetch.pickup
         private async Task<List<ConsigmentRequest>> GetConsignmentRequest()
         {
             var consigmentRequests = new List<ConsigmentRequest>();
-            bool more = true;
-            int count = 0;
-            int page = 1;
-            int size = 20;
+            var more = true;
+            var page = 1;
+            var size = 20;
 
             while (more)
             {
@@ -113,10 +100,10 @@ namespace ezisend.fetch.pickup
                 }
                 Console.WriteLine($"Consigment Request count: {consigmentRequests.Count} .....");
 
-                count = JObject.Parse(output).SelectToken("_count").Value<int>();
+                var count = JObject.Parse(output).SelectToken("_count").Value<int>();
                 page = JObject.Parse(output).SelectToken("_page").Value<int>();
                 size = JObject.Parse(output).SelectToken("_size").Value<int>();
-                if ((page * size) >= count)
+                if (page * size >= count)
                 {
                     more = false;
                 }
@@ -152,8 +139,8 @@ namespace ezisend.fetch.pickup
    		        ]
 	        }}";
 
-            var content = new StringContent(query.ToString(), Encoding.UTF8, "application/json");
-            var requestUri = $"/api/rts-dashboard/pickup";
+            var content = new StringContent(query, Encoding.UTF8, "application/json");
+            var requestUri = "/api/rts-dashboard/pickup";
             var response = await m_rtsClient.PostAsync(requestUri, content);
 
             Console.WriteLine($"RequestUri: {response.RequestMessage.RequestUri}");
@@ -199,31 +186,27 @@ namespace ezisend.fetch.pickup
 
         private async Task<bool> PostRtsPickupFormat(RtsPickupFormat rtsPickupFormat, bool doPosting)
         {
-            var success = true;
+            if (!doPosting) return true;
+            Console.WriteLine($"Posting: {rtsPickupFormat.ConsignmentNo}{rtsPickupFormat.PickupNo} {rtsPickupFormat.AccountNo}");
 
-            if (doPosting)
+            var json = JsonConvert.SerializeObject(rtsPickupFormat);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var requestUri = "/api/rts-pickup-formats";
+            var response = await m_ostClient.PostAsync(requestUri, content);
+
+            Console.WriteLine($"RequestUri: {response.RequestMessage.RequestUri}");
+            Console.WriteLine($"Status: {(int)response.StatusCode} {response.ReasonPhrase}");
+            if (!response.IsSuccessStatusCode)
             {
-                Console.WriteLine($"Posting: {rtsPickupFormat.ConsignmentNo}{rtsPickupFormat.PickupNo} {rtsPickupFormat.AccountNo}");
-
-                var json = JsonConvert.SerializeObject(rtsPickupFormat);
-                var content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
-                var requestUri = $"/api/rts-pickup-formats";
-                var response = await m_ostClient.PostAsync(requestUri, content);
-
-                Console.WriteLine($"RequestUri: {response.RequestMessage.RequestUri}");
-                Console.WriteLine($"Status: {(int)response.StatusCode} {response.ReasonPhrase}");
-                if (!response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine("Aborting .....");
-                    return false;
-                }
-
-                var output = await response.Content.ReadAsStringAsync();
-                var rtsPickupFormatId = JObject.Parse(output).SelectToken("$.id").Value<string>();
-                Console.WriteLine($"Posted: {rtsPickupFormatId}");
+                Console.WriteLine("Aborting .....");
+                return false;
             }
 
-            return success;
+            var output = await response.Content.ReadAsStringAsync();
+            var rtsPickupFormatId = JObject.Parse(output).SelectToken("$.id").Value<string>();
+            Console.WriteLine($"Posted: {rtsPickupFormatId}");
+
+            return true;
         }
     }
 }
